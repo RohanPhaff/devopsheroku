e51b72c6a42600b30b9320c3d996866ab7fc0efb
Rohan Phaff <phaf0001@hz.nl>
2022-06-24 20:54:47 +0200

    Update create.blade.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,15 +4,12 @@
 <form method="POST" action="/home">
     @csrf
 
-    <label for="name">Name</label><br>
     <input type="text" id="name" placeholder="Enter Name" name="name" maxlength="150"
         required>
 
-    <label for="complaint">Complaint</label><br>
     <input type="text" id="complaint" placeholder="Enter Complaint" name="complaint" maxlength="150"
         required>
 
-    <label for="date">Date</label><br>
     <input type="date" id="date" placeholder="Enter Date" name="date" maxlength="150"
         required>
 
